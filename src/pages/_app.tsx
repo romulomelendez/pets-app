d68e8ee263b47e789f08935ef8e9145e5e96aad0
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-06-05 08:18:08 -0300

    creating a theme with material-ui lib

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../ui/styles/globals.css'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { ThemeProvider } from '@mui/material'
+
+import '../ui/styles/globals.css'
+
+import theme from '../ui/themes/theme'
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+
+  return (
+
+    <ThemeProvider theme={ theme }>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  
+  )
 }
 
 export default MyApp
