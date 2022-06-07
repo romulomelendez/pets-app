928a7fb5d27143c3deed0ec14089f8cb41049fbb
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-06-07 07:04:41 -0300

    creating a service

---
+++ src/data/services/TextService.ts
@@ -0,0 +1,10 @@
+export const TextService = {
+
+    limitText( text: string, maxLength: number ): string {
+
+        if ( text.length < maxLength )
+            return text
+        return text.slice(0, maxLength) + '...'
+    }
+
+}
